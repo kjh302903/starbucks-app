f92a10a60f315a3d0574b8763b5ed0300b341f2d
kjh302903 <kjh302903@gmail.com>
2023-01-23 13:01:08 +0900

    Add badges

--- js/main.js
+++ js/main.js
@@ -14,3 +14,20 @@ searchInputEl.addEventListener('blur', () => {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(()=> {
+    if(window.scrollY > 500) {
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+    }
+    else {
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+    }
+}, 300));
